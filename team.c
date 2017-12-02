9d72bdfe5ed5d0fcd9beb52e93ed03391ed6cbed
Matt <msb217@lehigh.edu>
2017-12-02 14:37:23 -0500

    Initial

---
+++ team.c
@@ -0,0 +1,13 @@
+#include "support.h"
+
+/*
+ * Please be sure to fill in this struct right away!
+ */
+struct team_t team =
+{
+  /*No second team member*/
+  "Matthew Buell", /* first member name, e.g., Michael Spear */
+  "msb217@lehigh.edu", /* first member email, e.g., mfs409@lehigh.edu */
+  "c", /* second member name, e.g., Michael Spear */
+  "d"  /* second member email, e.g., mfs409@lehigh.edu */
+};
